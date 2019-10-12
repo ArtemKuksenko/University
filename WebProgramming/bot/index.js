3a47129a8d81ff9475c48c93712c956a77ab5f9c
Kuksenko Artem <kuksenko.artem@gmail.com>
2019-10-12 16:55:02 +0300

    Почистил немножк

--- WebProgramming/bot/index.js
+++ WebProgramming/bot/index.js
@@ -1,32 +1,57 @@
-//https://habr.com/ru/sandbox/115246/ - как запустить бота
-// Подключаем библиотеку для работы с Telegram API в переменную
-var TelegramBot = require('node-telegram-bot-api');
+/**
+ *  Модуль для взаимодействия бота тех поддержки
+ *      обход блокировок: https://habr.com/ru/sandbox/115246/
+ *      полезности: https://core.telegram.org/bots/api#getupdates
+ */
 
-// Устанавливаем токен, который выдавал нам бот
-var token = '649101019:AAG3MkoP_U0XsQ9qCqB4YGCNCcrP0RWU0bI';
-// Включить опрос сервера. Бот должен обращаться к серверу Telegram, чтобы получать актуальную информацию
-// Подробнее: https://core.telegram.org/bots/api#getupdates
+let TelegramBot = require('node-telegram-bot-api');
+const token = '649101019:AAG3MkoP_U0XsQ9qCqB4YGCNCcrP0RWU0bI';
+const WebSocket = require('ws');
+const MongoClient = require("mongodb").MongoClient;
+const mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true, useUnifiedTopology: true } );
+
+mongoClient.connect(function(err, cur){
+    const db = cur.db("SupportDB");
+    const collection = db.collection("worker");
+});
+
+let wss = new WebSocket.Server({
+  port: 8012
+});
+
+wss.broadcast = function(data, clientValidator = () => true) {
+    this.clients.forEach(client => {
+        if (clientValidator(client)) {
+            client.send(data);
+        }
+    });
+}
+
 var bot = new TelegramBot(token, {
     polling: true,
     request: {
         proxy: "http://localhost:8118"
-//        strictSSL: false
     }
 });
 
-// Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием, то есть "Hello World!")
-bot.onText(/\/echo (.+)/, function (msg, match) {
-console.log('echo')
-    var fromId = msg.from.id; // Получаем ID отправителя
-    var resp = match[1]; // Получаем текст после /echo
+// var fromId = null;
+
+bot.onText(/\/registrateWorker (.+)/, function (msg, match) {
+console.log('start_working')
+    let fromId = msg.from.id;
+
+    var resp = match[1];
     bot.sendMessage(fromId, resp);
+});
+
+wss.on("connection", ws => {
+    ws.on('message', message => {
+        console.log('пришло',message);
+        bot.sendMessage(fromId,message);
+    });
 });
 
 // Простая команда без параметров
 bot.on('message', function (msg) {
-console.log('echo')
-    var chatId = msg.chat.id; // Берем ID чата (не отправителя)
-    // Фотография может быть: путь к файлу, поток (stream) или параметр file_id
-    var photo = 'cat.jpeg'; // в папке с ботом должен быть файл "cats.png"
-    bot.sendPhoto(chatId, photo, { caption: 'Милые котята' });
+    wss.broadcast(msg.text)
 });
