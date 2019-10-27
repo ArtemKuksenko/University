8e10443a7e096576b94306f8e2ee485f285a1961
Kuksenko Artem <kuksenko.artem@gmail.com>
2019-10-27 12:34:34 +0300

    Сломал

--- WebProgramming/bot/index.js
+++ WebProgramming/bot/index.js
@@ -10,8 +10,8 @@ let wss = new WebSocket.Server({
     port: 8034
 });
 
-let usersList = [];
-let workerList = [];
+let wsListByWorker = [];
+let workerListByWs = [];
 const chatBotAdmin = 485915563;
 
 wss.broadcast = function(data, clientValidator = () => true) {
@@ -41,34 +41,40 @@ mongoClient.connect(async function(err, cur){
         }
     });
 
-    bot.onText(/\/help()/,(sender,msg) => {
-       bot.sendMessage(sender.from.id,
-            "/registration 'псвевдоним' - зарегаться,\n" +
-            "/score - счет" +
-            "/start - начать работать" +
-            "/end - закончить работу" +
-            "/leave - покинуть нас")
-    });
 
-    bot.onText(/\/score/,async (sender,msg) => {
-       let res = await collection.findOne({id:sender.from.id},{score: 1})
-       bot.sendMessage(sender.from.id, "твой счет: "+res.score)
-        console.log(res)
-    });
 
-    bot.onText(/\/start/,(sender,msg) => {
-        collection.update({id:sender.from.id},{$set:{work:true}})
-       bot.sendMessage(sender.from.id, "жди сообщений )")
-    });
-    bot.onText(/\/end/,(sender,msg) => {
-        collection.update({id:sender.from.id},{$set:{work:false}})
-       bot.sendMessage(sender.from.id, "тишина и покой, не будет писем пока")
-    });
+});
+bot.onText(/\/help/,(sender,msg) => {
+    bot.sendMessage(sender.from.id,
+        "/registration 'псвевдоним' - зарегаться,\n" +
+        "/score - счет" +
+        "/start - начать работать" +
+        "/end - закончить работу" +
+        "/leave - покинуть нас")
 
-    bot.onText(/\/leave()/,async (sender,msg) => {
-        let res = await collection.remove({id:sender.from.id});
-        if (res.result.n)
-            bot.sendMessage(sender.from.id, "Пока,\nНадеюсь ты нашел работу по лучше)");
+bot.onText(/\/score()/,async (sender,msg) => {
+let res = await collection.findOne({id:sender.from.id},{score: 1})
+bot.sendMessage(sender.from.id, "твой счет: "+res.score)
+console.log(res)
+});
+
+bot.onText(/\/start()/,(sender,msg) => {
+collection.update({id:sender.from.id},{$set:{work:true}})
+bot.sendMessage(sender.from.id, "жди сообщений )")
+});
+bot.onText(/\/end()/,(sender,msg) => {
+collection.update({id:sender.from.id},{$set:{work:false}})
+bot.sendMessage(sender.from.id, "тишина и покой, не будет писем пока")
+});
+
+bot.onText(/\/leave()/,async (sender,msg) => {
+let res = await collection.remove({id:sender.from.id});
+if (res.result.n)
+// Простая команда без параметров
+    bot.on("message", function (msg) {
+        wsListByWorker[msg.from.id].send(msg.text);
+    });
+bot.sendMessage(sender.from.id, "Пока,\nНадеюсь ты нашел работу по лучше)");
         else
             bot.sendMessage(sender.from.id, "Ты и так не с нами,\nЧто мертво - умереть не может\n(c)Грейджои из замка Пайк");
     });
@@ -87,6 +93,16 @@ mongoClient.connect(async function(err, cur){
         }
     });
 
+    bot.onText(/\/closeDialog()/,async (msg)=> {
+        let worker = msg.from.id;
+        let ws = wsListByWorker[worker]
+        await collection.update({id:worker},{$set:{busy:false}});
+        await cycle.remove({id:worker});
+        delete workerListByWs[ws];
+        delete wsListByWorker[worker];
+        ws.close();
+    });
+
     wss.on("connection", ws => {
         console.log('connect');
         ws.on('message', async (message) => {
@@ -102,36 +118,39 @@ mongoClient.connect(async function(err, cur){
 
                 }
 
+            } else {
+                let select = await cycle.findOne({token:message.token});
+                if (select === null){
+                    let res = await collection.find({work: true}).toArray();
+                    let worker = res[Math.round( Math.random() * (res.length -1) )].id;
+                    console.log('stop')
+                    res = await collection.update({id:worker},{$set:{busy:true}});
+                    let ins = await cycle.insert({'worker': worker, 'token':message.token});
+                    bot.sendMessage(worker, message.text);
+                    wsListByWorker[worker] = ws;
+                    workerListByWs[ws] = worker;
+                }   else
+                    bot.sendMessage(select.worker, message.text);
             }
-            let select = await cycle.find({token:message.token}).toArray();
-            if (select.length === 0){
-                let res = await collection.find({work: true}).toArray();
-                let worker = res[Math.round( Math.random() * (res.length -1) )].id;
-                console.log('stop')
-                res = await collection.update({id:worker},{$set:{busy:true}});
-                // db.worker.update({id:485915563},{$set:{busy:false}})
-                let ins = await cycle.insert({'ws': ws, 'worker': worker, 'token':message.token});
-                if (ins.result.n && res)
-                    console.log(ins);
-                bot.sendMessage(worker, message.text);
-                // usersList[message.token] = {'ws': ws, 'worker': worker};
-                // workerList[worker] = ws;
-            }   else
-                bot.sendMessage(select[0].worker, message.text);
+
         });
 
-    });
-     wss.on('close', async function(ws) {
-         await collection.updated({ws:ws},{$set:{busy:false}});
-         await cycle.remove({ws:ws})
-         ws.close();
-    // close user connection
-        });
 
-// Простая команда без параметров
-    bot.on('message', function (msg) {
-        workerList[msg.from.id].send(msg.text);
     });
+    // closeConnection = async (ws) => {
+    //      let worker = workerListByWs[ws];
+    //      await collection.update({id:worker},{$set:{busy:false}});
+    //      await cycle.remove({id:worker});
+    //      delete workerListByWs[ws];
+    //      delete wsListByWorker[worker];
+    //      ws.close();
+    // }
+    // wss.on('close', closeConnection(ws));
+    // wss.on('disconnect', closeConnection(ws));
+    // socket.on('error', function (ws,err) {
+    //     console.log(ws)
+    // });
+
 
 
 
